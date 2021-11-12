5be6b45f1032f744db1989ff8d62f9d8eff08050
Iván Masías <ivan.masias.ortiz@gmail.com>
2021-11-12 01:08:21 -0300

    [ADD]
    se agrega opción automática de instalación

---
+++ vivaldi_browser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+function update(){
+	apt update
+	apt upgrade -y
+	apt autoremove -y
+}
+
+update
+
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
+
+if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
+text="deb ${archtype} https://repo.vivaldi.com/archive/deb/ stable main"
+
+echo $text >> /etc/apt/sources.list.d/vivaldi.list
+
+update
+
+apt install vivaldi-stable -y
