54a2b28c0568ca2de932602ae81ce6e63c4dde8c
Iván Masías <ivan.masias.ortiz@gmail.com>
2021-12-13 12:54:08 -0300

    [ADD]
    permisos para dockers, se deja comentado compose

--- install_dockers.sh
+++ install_dockers.sh
@@ -14,3 +14,11 @@ wget -qO- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+#Permisos a Usuario en Modo Super Usuario
+usermod -aG docker $USER
+newgrp - docker
+
+#Para instalar compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
