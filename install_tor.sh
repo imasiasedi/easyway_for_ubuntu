5aa6d6a5936e49e6735cb2a8cef58a5a0c675d50
Iván Masías <ivan.masias.ortiz@gmail.com>
2021-12-13 12:56:52 -0300

    [ADD]
    tor install

---
+++ install_tor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function update(){
+	apt update
+	apt upgrade -y
+	apt autoremove -y
+}
+
+update
+
+if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
+text="deb ${archtype} https://deb.torproject.org/torproject.org/ $(lsb_release -cs) main"
+echo $text >> /etc/apt/sources.list.d/tor.list
+wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | apt-key add -
+
+update
+sudo apt install tor tor-geoipdb torsocks deb.torproject.org-keyring -y
