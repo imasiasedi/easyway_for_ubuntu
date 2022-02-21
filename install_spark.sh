c30959682eb6e990c72961b4f88b02d5526cb4b2
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-02-21 11:43:56 -0300

    [ADD] Install spark
    - instalador automatico de spark

---
+++ install_spark.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function update(){
+	apt update
+	apt upgrade -y
+	apt autoremove -y
+}
+
+update
+
+add-apt-repository ppa:linuxuprising/java -y
+update
+wget https://dlcdn.apache.org/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
+tar xvf spark-3.2.1-bin-hadoop3.2.tgz
+sudo mv spark-3.2.1-bin-hadoop3.2/ /opt/spark
+chmod 777 -R /opt/spark
+
+apt install default-jdk git -y
+
+echo "export SPARK_HOME=/opt/spark" >> ~/.bashrc
+echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bashrc
+echo "export PYSPARK_PYTHON=/usr/bin/python3" >> ~/.bashrc
+source ~/.bashrc
+
+#parte el servicio
+start-master.sh
