a19cba7aadda18684f0260999250c2559c436944
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-02-28 13:51:39 -0300

    [UPDATE] actulización de script
    - se actualiza el script con función y mejoras en visualización

--- github_desktop.sh
+++ github_desktop.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 function update(){
-	apt update
-	apt upgrade -y
-	apt autoremove -y
+	apt-get update -y
+	apt-get upgrade -y
+	apt-get autoremove -y
 }
 
-update
+update > /dev/null
 
-wget -qO- https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
-if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
-text="deb ${archtype} https://packagecloud.io/shiftkey/desktop/any/ any main"
-echo $text >> /etc/apt/sources.list.d/shiftkey-desktop.list
+function install_gith_desk(){
+	echo "---> Creando APT Source  ... "
+	text="deb ${archtype} https://packagecloud.io/shiftkey/desktop/any/ any main"
+	echo $text >> /etc/apt/sources.list.d/shiftkey-desktop.list
+	echo "---> Github Desktop Key ... "
+	wget -qO- https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
+	echo "---> Actualizando ... "
+	update > /dev/null
+	echo "---> Instalando Paquetes ... "
+	apt-get install github-desktop -y > /dev/null
+	echo ""
+}
 
-update
-
-apt install github-desktop -y
+echo "Instalando VScode ..."
+install_gith_desk
+echo "Enjoy 3:)"
