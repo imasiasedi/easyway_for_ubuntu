a3e285016767a46d4be94b1f510716b04e0f2366
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-03-12 23:39:45 -0300

    [ADD] Mejoras en función de instalación

--- install_dockers.sh
+++ install_dockers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 function update(){
-	apt update
-	apt upgrade -y
-	apt autoremove -y
+	apt-get update -y
+	apt-get upgrade -y
+	apt-get autoremove -y
 }
 
-update
-
+update > /dev/null
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
+
 text="deb ${archtype} https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 echo $text >> /etc/apt/sources.list.d/docker.list
 wget -qO- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
--- install_tor.sh
+++ install_tor.sh
@@ -6,8 +6,8 @@ function update(){
 }
 
 update > /dev/null
-
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
+
 text="deb ${archtype} https://deb.torproject.org/torproject.org/ $(lsb_release -cs) main"
 echo $text >> /etc/apt/sources.list.d/tor.list
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | apt-key add -
