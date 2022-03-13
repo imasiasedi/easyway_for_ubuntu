897bc5dd5874506c6d6a90ab0e1715854199068c
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-03-12 23:38:21 -0300

    [ADD] mejora en script
    - se crean funciones y mejora el código de instalación

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 function update(){
-	apt update
-	apt upgrade -y
-	apt autoremove -y
+	apt-get update -y
+	apt-get upgrade -y
+	apt-get autoremove -y
 }
 
-update
-
-wget -qO- https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+update > /dev/null
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
-text="deb ${archtype} http://dl.google.com/linux/chrome/deb/ stable main"
-echo $text >> /etc/apt/sources.list.d/google-chrome.list
 
-update
+function install_chrome(){
+	echo "---> Creando APT Source  ... "
+	text="deb ${archtype} http://dl.google.com/linux/chrome/deb/ stable main"
+	echo $text >> /etc/apt/sources.list.d/google-chrome.list
+	echo "---> Chrome Key ... "
+	wget -qO- https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	echo "---> Actualizando ... "
+	update > /dev/null
+	echo "---> Instalando Paquetes ... "
+	apt-get install google-chrome-stable -y > /dev/null
+	echo ""
+}
 
-apt install google-chrome-stable -y
+echo "Instalando Chrome ..."
+install_chrome
+echo "Enjoy 3:)"
