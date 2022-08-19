2ec3c2ff356e66db85adabbdeca7352d879a49e2
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-08-19 01:05:37 -0400

    [UPDATE] actualizacion en compose y dockers

--- install_dockers.sh
+++ install_dockers.sh
@@ -5,20 +5,37 @@ function update(){
 	apt-get autoremove -y
 }
 
+USER="${SUDO_USER:-${USER}}"
+source_list=/etc/apt/sources.list.d
+gpgkey_path=/etc/apt/trusted.gpg.d
+
 update > /dev/null
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
 
-text="deb ${archtype} https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-echo $text >> /etc/apt/sources.list.d/docker.list
-wget -qO- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+function install_docker(){
+	echo "---> Creando APT Source  ... "
+	text="deb ${archtype} https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	echo $text >> $source_list/docker.list
+	echo "---> Docker Key ... "
+	wget -qO- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmour > $gpgkey_path/docker.gpg
+	echo "---> Actualizando ... "
+	update > /dev/null
+	echo "---> Instalando Paquetes ... "
+	apt-get install docker-ce docker-ce-cli containerd.io -y > /dev/null
+	echo "---> Permisos Usuario ... "
+	groupadd docker
+	usermod -aG docker $USER
+	chmod 666 /var/run/docker.sock
+	echo ""
+}
 
-update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+function install_compose(){
+	#Para instalar compose
+	wget -qO /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/v2.9.0/docker-compose-linux-x86_64
+	chmod +x /usr/local/bin/docker-compose
+}
 
-#Permisos a Usuario en Modo Super Usuario
-usermod -aG docker $USER
-newgrp - docker
-
-#Para instalar compose
-#sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
+echo "Instalando Dockers ..."
+install_docker
+install_compose
+echo "Enjoy 3:)"
