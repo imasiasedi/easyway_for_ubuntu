17b603bf5deefc1f8a147f15611574962af7e5ee
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-10-20 16:13:41 -0300

    [UPDATE] paquetes adicionales

--- install_lutris.sh
+++ install_lutris.sh
@@ -10,6 +10,8 @@ LTRS=lutris-all.deb
 function install_lutris(){
 	echo "---> Descargando Paquetes  ... "
 	wget -qO $LTRS https://github.com/lutris/lutris/releases/download/v0.5.12-beta1/lutris_0.5.12_beta1_all.deb
+	echo "---> Instalando Paquetes Adicionales ... "
+	apt install curl fluid-soundfont-gm fluid-soundfont-gs mesa-utils mesa-utils-bin p7zip python3-bs4 python3-html5lib python3-lxml python3-magic python3-setproctitle python3-soupsieve python3-webencodings -y > /dev/null
     echo "---> Install ... "
 	dpkg -i $LTRS > /dev/null
 	echo "---> Actualizando ... "
