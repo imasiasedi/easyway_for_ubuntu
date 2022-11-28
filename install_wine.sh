968cdce854178cece5e93bf989a41501aa38a263
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-11-28 00:33:24 -0300

    [ADD] se agregan wine

---
+++ install_wine.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+function update(){
+	apt-get update -y
+	apt-get upgrade -y
+	apt-get autoremove -y
+}
+
+release=$(lsb_release -cs)	#Versión de SO (solo ubuntu)
+source_list=/etc/apt/sources.list.d
+gpgkey_path=/etc/apt/trusted.gpg.d
+
+update > /dev/null
+if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
+
+function install_winehq(){
+	echo "---> Creando APT Source  ... "
+	wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/$release/winehq-$release.sources
+	echo "---> WineHQ Key ... "
+	mkdir -pm755 /etc/apt/keyrings
+    wget -qO- /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+	
+	echo "---> Actualizando ... "
+	update > /dev/null
+	echo "---> Instalando Paquetes ... "
+	dpkg --add-architecture i386 
+	apt install --install-recommends winehq-stable -y > /dev/null
+	echo ""
+}
+
+echo "Instalando WineHQ ..."
+install_winehq
+echo "Enjoy 3:)"
