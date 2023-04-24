3bb724ea595018ce89c509dadca3d43309ab1c09
Iván Masías <ivan.masias.ortiz@gmail.com>
2023-04-23 23:58:00 -0400

    [UPDATE] Actualizara versión 3.2.1

--- github_desktop.sh
+++ github_desktop.sh
@@ -5,16 +5,17 @@ function update(){
 	apt-get autoremove -y
 }
 
+version=GitHubDesktop-linux-3.2.1-linux1.deb
 update > /dev/null
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
 
 function install_gith_desk(){
 	echo "---> Descargando Paquetes  ... "
-	wget -q https://github.com/shiftkey/desktop/releases/download/release-3.0.5-linux1/GitHubDesktop-linux-3.0.5-linux1.deb
+	wget -q https://github.com/shiftkey/desktop/releases/download/release-3.2.1-linux1/$version
 	echo "---> Instalando Paquetes  ... "
-	dpkg -i GitHubDesktop-linux-3.0.5-linux1.deb
+	dpkg -i $version
 	echo "---> Eliminando Paquetes ... "
-	rm GitHubDesktop-linux-3.0.5-linux1.deb
+	rm $version
 	echo ""
 }
 
